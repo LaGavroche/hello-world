055ca0d76b102111c863e1f70d6a9aa4d8a1047c
DENTAGEST\rayan.boiteau <rayan.boiteau@dentagest.ch>
2025-05-02 09:10:39 +0200

    smart contract done

--- scripts/interact.js
+++ scripts/interact.js
@@ -1,24 +1,30 @@
 const hre = require("hardhat");
+
 async function main() {
- // Remplacez par l'adresse de votre contrat déployé
- const contractAddress = "0xA16FD066Bc82E116AbE9eA09716E22fa7C7bC5B2";
- // On se connecte à notre contrat existant
- const HelloWorld = await hre.ethers.getContractFactory("HelloWorld");
- const hello = await HelloWorld.attach(contractAddress);
- // On lit le message actuel
- const currentMessage = await hello.message();
- console.log("Message actuel :", currentMessage);
- // On met à jour le message
- const tx = await hello.update("Nouveau message !");
- await tx.wait();
- console.log("Message mis à jour !");
-
- // On vérifie que le message a bien été mis à jour
- const newMessage = await hello.message();
- console.log("Nouveau message :", newMessage);
+  // Remplacez par l'adresse de votre contrat déployé
+  const contractAddress = "0x77bd66bcE03C92266C49C6C79A6ED6e024B49FDb";
+  // On se connecte à notre contrat existant
+  const HelloWorld = await hre.ethers.getContractFactory("HelloWorld");
+  const hello = await HelloWorld.attach(contractAddress);
+  
+  // Mon prénom comme donnée
+  const prenom = "Rayan";
+  
+  // On lit le message actuel
+  const currentMessage = await hello.message();
+  console.log("Message actuel :", currentMessage);
+  
+  // On met à jour le message avec mon prénom
+  const tx = await hello.update(`Nouveau message de ${prenom} !`);
+  await tx.wait();
+  console.log("Message mis à jour avec mon prénom !");
+ 
+  // On vérifie que le message a bien été mis à jour
+  const newMessage = await hello.message();
+  console.log("Nouveau message :", newMessage);
 }
-main().catch((error) => {
- console.error(error);
- process.exitCode = 1;
-});
 
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
